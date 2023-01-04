7bb2e16d66b3da73d0c1142c372a550ce4925941
FuadAbdullah <muhammadfuadbinabdullah@gmail.com>
2023-01-04 14:19:18 +0800

    Created sign in page

--- awerkout/signInPage.aspx
+++ awerkout/signInPage.aspx
@@ -1,5 +1,53 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/SiteFrame.Master" AutoEventWireup="true" CodeBehind="signInPage.aspx.cs" Inherits="awerkout.signInPage" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <style type="text/css">
+        .auto-style2 {
+            height: 26px;
+        }
+
+        .align-right {
+            text-align: right;
+        }
+    </style>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContentBlock" runat="server">
+    
+    <h1>Sign in to your Account</h1>
+
+    <table class="auto-style1">
+        <tr>
+            <td class="align-right">
+                <asp:Label ID="usernameLbl" runat="server" Text="Username:"></asp:Label>
+            </td>
+            <td>
+                <asp:TextBox ID="usernameTxtBx" runat="server"></asp:TextBox>
+            </td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td class="align-right">
+                <asp:Label ID="passwordLbl" runat="server" Text="Password:"></asp:Label>
+            </td>
+            <td>
+                <asp:TextBox ID="passwordTxtBx" runat="server" TextMode="Password"></asp:TextBox>
+            </td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td>
+                <asp:Button ID="signInBtn" runat="server" OnClick="signInBtn_Click" Text="Sign In" />
+            </td>
+            <td>
+                <asp:Label ID="generalErrorMsg" runat="server" ForeColor="Red" Text="General Error"></asp:Label>
+            </td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td class="auto-style2"></td>
+            <td class="auto-style2"></td>
+            <td class="auto-style2"></td>
+        </tr>
+    </table>
+
+
 </asp:Content>
--- awerkout/signInPage.aspx.cs
+++ awerkout/signInPage.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +13,88 @@ namespace awerkout
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            generalErrorMsg.Visible = false;
+        }
 
+        protected void signInBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Required to connect to the database
+                SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["myConn"].ConnectionString);
+                conn.Open();
+                // --
+
+                // Check if the username and password entry exist in the db
+                string query = "select count(*) from userData where username = '"
+                    + usernameTxtBx.Text
+                    + "' and password = '"
+                    + passwordTxtBx.Text
+                    + "'";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                int check = Convert.ToInt32(cmd.ExecuteScalar().ToString());
+
+                // If the database do have the record (> 0 record(s)), then username and password is correct
+                if (check > 0)
+                {
+
+                    // Fetch the first name and the user type
+                    string fetchUser = "select firstname, usertype, username from userData where username = '"
+                    + usernameTxtBx.Text
+                    + "' and password = '"
+                    + passwordTxtBx.Text
+                    + "'";
+
+                    SqlCommand cmdFetch = new SqlCommand(fetchUser, conn);
+                    SqlDataReader fetchResult = cmdFetch.ExecuteReader();
+
+                    // Placeholder to store the usertype and first name
+                    string type = "";
+                    string firstname = "";
+                    string username = "";
+
+                    // Will loop as long as Read is true, could indicate existence of multiple records
+                    // Loop will override with the last record in case there indeed are multiple records
+                    while (fetchResult.Read())
+                    {
+                        type = fetchResult["usertype"].ToString().Trim();
+                        firstname = fetchResult["firstname"].ToString().Trim();
+                        username = fetchResult["username"].ToString().Trim();
+                    }
+
+                    // Storing the first name and usertype
+                    Session["firstname"] = firstname;
+                    Session["usertype"] = type;
+                    Session["username"] = username;
+
+                    // Redirect admin to adminDashboard.aspx
+                    if (type == "ADMIN")
+                    {
+                        Response.Redirect("adminProfile.aspx");
+                    }
+                    // Redirect member to memberDashboard.aspx
+                    else if (type == "USER")
+                    {
+                        Response.Redirect("userProfile.aspx");
+                    }
+
+                }
+                else // No record fetched meaning no matching username and password were found in the db
+                {
+                    generalErrorMsg.Visible = true;
+                    generalErrorMsg.ForeColor = System.Drawing.Color.Red;
+                    generalErrorMsg.Text = "Incorrect username or password!";
+                }
+
+                // Must close connection once opened
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                generalErrorMsg.Visible = true;
+                generalErrorMsg.ForeColor = System.Drawing.Color.Red;
+                generalErrorMsg.Text = "Error: " + ex.ToString();
+            }
         }
     }
 }
